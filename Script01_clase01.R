af33818df00b05495814d0a119e4dd0b27f2e913
jcondor01 <jorge.condor@gmail.com>
2025-09-16 15:06:19 -0500

    Archivos creados en la estructura de posit

---
+++ Script01_clase01.R
@@ -0,0 +1,19 @@
+#promera sesión
+#En esta sesión mostraremos algunas de las capacidades del entorno 
+# de desarrollo integrado (IDE) posit(ext. RStudio)
+
+####Introduccipon la lenguaje de programación R. ####
+# R es un lenguaje de programación y entorno de software libre
+#diseñado especificamente para análisis de datos, visualisación de datos
+# modelado computacional.
+
+##### Origen ####
+#origen de los 90s
+#Objetivo inicial: Proporcionar una alternativa flexible y accesible al entorno
+# S
+
+####Reconocimiento actual####
+# Es uno de lso leguajes más utilizados en el análisis de datos 
+#junto con python
+
+#tiene miles de paquetes disponibles a través de CRAN
---
+++ Script02_clase01.R
@@ -0,0 +1,4 @@
+
+#### Herramientas en línea para programar con R ####
+#Posi.cloud
+#Notebook d Google Colab
